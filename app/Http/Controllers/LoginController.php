68cf85f8fbce18a8930928af590f1481eb8ff3cb
arun <arun@cyberoze.com>
2019-09-26 23:52:25 +0530

    recovered login controller

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Services\LoginService;
+use Validator;
+class LoginController extends Controller
+{
+    protected $loginService;
+	public function __construct(LoginService $loginService) {
+		
+		$this->loginService = $loginService;
+    } 
+    
+    public function signIn(Request $request){
+        $validator = Validator::make($request->all(), [
+            'otp' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->respondWithValidationError($validator);
+        }
+       
+        $user = $this->loginService->signIn($request);
+        if(!$user){
+            return $this->respondNotFound("invalid otp");                        
+        }
+        else{
+            return $this->respondWithSuccess($user);            
+        }
+        
+    }
+    public function signUp(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'mobile' => 'required|unique:users',
+            'email'  => 'required|unique:users',
+        ]);
+        if ($validator->fails()) {
+            return $this->respondWithValidationError($validator);
+        }
+        $user = $this->loginService->signUp($request);
+        return $this->respondWithSuccess($user);
+        
+    }
+    public function generateOtp(Request $request){
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->respondWithValidationError($validator);
+        }
+        $user = $this->loginService->generateOtp($request);
+        if(!$user){
+            return $this->respondWithError("Mobile number is not registered");                                    
+        }
+        else{
+            return $this->respondWithSuccess($user);  
+        }
+        
+    }
+}
